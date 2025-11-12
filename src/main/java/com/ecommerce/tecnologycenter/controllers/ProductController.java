9d11e917c6f571353d9a3225d38d6844024de999
Allan Oliveira <allan.oliveiraa009@gmail.com>
2025-11-12 16:42:58 -0300

    Insert product

--- src/main/java/com/ecommerce/tecnologycenter/controllers/ProductController.java
+++ src/main/java/com/ecommerce/tecnologycenter/controllers/ProductController.java
@@ -1,12 +1,14 @@
 package com.ecommerce.tecnologycenter.controllers;
 
 import com.ecommerce.tecnologycenter.dto.ProductDTO;
+import com.ecommerce.tecnologycenter.entities.Product;
 import com.ecommerce.tecnologycenter.services.ProductService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 //Esta anotation configura pra que quando a applicaçao rodar, o que tiver dentro desta classe vai responder pela web
 @RestController
@@ -28,6 +30,20 @@ public class ProductController {
         // Aqui ele é passado pra uma variavel do tipo DTO e o objeto é retornado
         ProductDTO dto = service.findById(id);
         return dto;
+    }
+
+    @GetMapping
+    // Pageable pra fazer uma busca paginada
+    public Page<ProductDTO> findAll(Pageable pageable){
+        // O metodo do CONTROLLER chama o metodo do service que busca no repositorio e ja tras a lista de objetos ProductDTO
+        return service.findAll(pageable);
+    }
+
+    // Pois vai ser um metodo de post
+    @PostMapping
+    // RequestBudy = Faz com que o corpo da requisição que foi enviado pelo front, entre no parametro e instancia um DTO correspondente
+    public ProductDTO insert(@RequestBody ProductDTO dto){
+        return service.insert(dto);
     }
 
 }
--- src/main/java/com/ecommerce/tecnologycenter/services/ProductService.java
+++ src/main/java/com/ecommerce/tecnologycenter/services/ProductService.java
@@ -4,10 +4,12 @@ import com.ecommerce.tecnologycenter.dto.ProductDTO;
 import com.ecommerce.tecnologycenter.entities.Product;
 import com.ecommerce.tecnologycenter.repositories.ProductRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
-import java.util.Optional;
+import java.util.List;
 
 @Service
 public class ProductService {
@@ -18,43 +20,53 @@ public class ProductService {
     // ReadOnly é pra não dar lock no banco de dados
     @Transactional(readOnly = true)
     public ProductDTO findById(Long id){
-        //FindById retorta um objeto do tipo OPTIONAL e por isso o resultado da busca é armazenado dentro de uma variavel
-        // deste tipo
+        /*
+        -> FindById retorta um objeto do tipo OPTIONAL e por isso o resultado da busca é armazenado dentro de uma variavel deste tipo
         Optional<Product> result = repository.findById(id);
 
-        //esse .get é responsavel por pegar o Objeto de dentro do OPTIONAL
-        // Depois de pegar esse objeto, ele é atribuido a variavel do tipo product, que poder pegar os dados que queremos
+        -> esse .get é responsavel por pegar o Objeto de dentro do OPTIONAL
+        -> Depois de pegar esse objeto, ele é atribuido a variavel do tipo product, que poder pegar os dados que queremos
         Product product = result.get();
 
-        //Instanciando e copiando os dados pro DTO
+        -> Instanciando e copiando os dados pro DTO
         ProductDTO dto = new ProductDTO(product.getId(), product.getName(), product.getDescription(), product.getPrice(),
                 product.getImgUri());
 
-        // E retorna esse dto, que vai ter os mesmos dados do produto
+        -> E retorna esse dto, que vai ter os mesmos dados do produto
         return dto;
+         */
 
-        /*
-        -> JEITO RESUMIDO
-
+        //Com intuido de nn precisar passar todos os dados como parametro no construtor, fiz um novo construtor no ProductDTO
+        //Que recebe um objeto de uma vez so pega e clona todos os dados do produto pro produtoDTO
         Product product = repository.findById(id).get();
         return new ProductDTO(product);
-
-        */
     }
 
-    /*
-    Com intuido de nn precisar passar todos os dados como parametro no construtor, fiz um novo construtor no ProductDTO
-    Que recebe um objeto de uma vez so pega e clona todos os dados do produto pro produtoDTO
-
-    public ProductDTO findById(Long id){
-
-        Optional<Product> result = repository.findById(id);
-
-        Product product = result.get();
-
-        ProductDTO dto = new ProductDTO(product);
-        return dto;
+    @Transactional(readOnly = true)
+    public Page<ProductDTO> findAll(Pageable pageable){
+        // Pegando todos os registros referentes a produto e colocando em uma lista
+        Page<Product> results = repository.findAll(pageable);
+        //Page é um tipo de lista paginada, nn precisa mais colocar STREAM pq o PAGE ja é um STREAM
+        return results.map(x -> new ProductDTO(x));
     }
-    */
+
+    // Como vai inserir algo no banco, não é operação somente de leitura, ai é necessario tirar o READ-ONLY
+    @Transactional
+    public ProductDTO insert(ProductDTO dto){
+
+        //Instanciamos o objeto e passamos os dados vindos da requisição pro objeto
+        Product entity = new Product();
+        entity.setName(dto.getName());
+        entity.setDescription(dto.getDescription());
+        entity.setPrice(dto.getPrice());
+        entity.setImgUri(dto.getImgUrl());
+
+        //Salvando no banco
+        entity = repository.save(entity);
+
+        //Reconverto pra DTO pra poder retornar no metodo
+        return new ProductDTO(entity);
+    }
+
 
 }
